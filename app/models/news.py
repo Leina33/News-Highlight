0858f89731ae011a6a7377da3facb52ccb594bcb
Leina33 <dominickrutto@gmail.com>
2019-11-18 12:09:54 +0300

    This is the process results function

---
+++ app/models/news.py
@@ -0,0 +1,10 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,id,title,overview):
+        self.id =id
+        self.title = title
+        self.overview = overview
+
